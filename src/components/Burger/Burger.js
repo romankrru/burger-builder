315a7e9c2a9f473cffdc0796e16f318e0184b071
Roman <rm07ru@gmail.com>
2017-12-01 22:04:52 +0300

    Add ingridients sum calculation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,23 @@ import Ingridient from './BurgerIngridient/BurgerIngridient';
 import styles from './Burger.css';
 
 const Burger = (props) => {
-  const transformedIngridients = (
+  let transformedIngridients = (
     Object.keys(props.ingridients)
     .map((ingridientKey) => {
       return [...Array(props.ingridients[ingridientKey])].map((_, i) => {
         return <Ingridient key={ingridientKey + i} type={ingridientKey} />
       });
     })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, [])
   );
+
+  if (transformedIngridients.length === 0) {
+    transformedIngridients = <p>Please start adding ingridients!</p>
+  }
+
+  console.log(transformedIngridients)
 
   return (
     <div className={styles.Burger}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,11 +6,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   state = {
     ingridients: {
-      cheese: 1,
-      salad: 1,
-      bacon: 2,
-      meat: 1,
-      cheese: 2
+      salad: 0,
+      bacon: 0,
+      meat: 0,
+      cheese: 0
     }
   }
 
