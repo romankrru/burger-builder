b1493ab528d1519c2ff9f8e8e4ed4afc2d9ec3a5
Roman <rm07ru@gmail.com>
2017-12-18 20:13:49 +0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  app,
   document.getElementById('root'),
 );
 
---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+import * as actionTypes from './actions';
+
+const initialState = {
+  ingridients: null,
+  totalPrice: 4,
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
