b0424891cd7eb2ee654e3b6648565369f8975f97
Roman <rm07ru@gmail.com>
2018-01-07 18:45:43 +0300

    Add test suits for BurgerBuilder container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { BurgerBuilder } from './BurgerBuilder';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+configure({
+  adapter: new Adapter(),
+});
+
+
+describe('<BurgerBuilder />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<BurgerBuilder
+      onIngredientInited={() => { }}
+      price={0}
+      history={{}}
+      onIngridientAdded={() => { }}
+      onIngridientRemoved={() => { }}
+    />);
+  });
+
+  it('should render <BuildControls /> if props.ings passed', () => {
+    wrapper.setProps({
+      ings: { slad: 0 },
+    });
+    expect(wrapper.find(BuildControls)).toHaveLength(1);
+  });
+});
